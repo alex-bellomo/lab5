bce3093f56ff9067a1e3ce990f6c54eca363d98c
Alex Bellomo <alex.bellomo.ab@gmail.com>
2021-02-04 21:28:46 -0500

    Chores model passing all tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,4 +2,21 @@ class Chore < ApplicationRecord
     has_many :tasks
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :alphabetical, -> { order(:name) }
+    scope :by_task, -> { joins(:task).order("tasks.name") }
+    scope :done, -> { where(completed:true) }
+    scope :pending, -> { where(completed:false) }
+    scope :chronological, -> { order("due_on", "completed")}
+    scope :upcoming, -> { where("due_on >= ?", Date.current) }
+    scope :past, -> { where("due_on < ?", Date.current) }
+
+    def status
+        if (completed)
+            return "Completed"
+        end
+        return "Pending"
+    end
 end
